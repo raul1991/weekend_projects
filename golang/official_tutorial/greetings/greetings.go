9f24cbd3a8f9d0fcd600e22e266f14f6cf9290e4
Rahul Bawa <supremeanitabawa@gmail.com>
2021-09-11 23:39:53 +0530

    golang: More exercises

---
+++ golang/official_tutorial/greetings/greetings.go
@@ -0,0 +1,17 @@
+package greetings
+
+import (
+"fmt"
+"errors"
+)
+
+// Hello returns a greeting for the named person
+func Hello(name string) (string, error) {
+  // If no name is given, return an error
+  if name == "" {
+    return "", errors.New("empty name")
+  }
+  // Return a greeting that embeds the name in a message.
+  message := fmt.Sprintf("Hi, %v. Welcome!", name)
+  return message, nil
+}
